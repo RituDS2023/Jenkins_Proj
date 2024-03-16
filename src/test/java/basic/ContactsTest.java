f618e5e697ec6dff46f3b649f3df9e9eb130df2b
RituDS2023 <150459040+RituDS2023@users.noreply.github.com>
2024-03-16 13:15:25 +0530

    updated the project

---
+++ src/test/java/basic/ContactsTest.java
@@ -0,0 +1,15 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ContactsTest {
+	@Test
+	public void createContactsTest()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.ajio.com/");
+	}
+
+}
